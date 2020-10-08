88de289dd47a5545536b809975ba14c9ef3a55c4
AustinFlap <56319429+AustinFlap@users.noreply.github.com>
2020-10-09 00:55:45 +0200

    Add files via upload

---
+++ includes/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/15 14:31:18 by avieira           #+#    #+#             */
+/*   Updated: 2020/10/09 00:46:24 by austin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define MAX_FD 250
+# define BUFFER_SIZE 1024
+
+size_t			ft_strl_spec(const char *str, char spec);
+int				get_next_line(int fd, char **line);
+
+#endif
