094178484995130f2e3a13e5cb697db663d3c5c8
Austin VIEIRA <avieira@e2r7p16.42.fr>
2020-10-21 17:43:50 +0200

    cd done, missing absolute path in g_env paths

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,17 +6,33 @@
 /*   By: tlecoeuv <tlecoeuv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 14:12:41 by tlecoeuv          #+#    #+#             */
-/*   Updated: 2020/10/21 16:29:45 by avieira          ###   ########.fr       */
+/*   Updated: 2020/10/21 17:40:23 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void		manage_env_path(char *pwd)
+{
+	char	*oldpwd;
+
+	if (!(oldpwd = getcwd(NULL, 0)))
+		return ;
+	set_env_var("OLDPWD", oldpwd);
+	set_env_var("PWD", pwd);
+}
+
 void		ft_cd(char **args)
 {
-	if (args[1] == NULL)
-		printf("pas encore implementer le path home\n");
+	char	*str;
+	int		size;
+
+	size = get_array_size(args) - 1;
+	if (size && ft_strcmp("~", args[size]))
+			str = args[1];
 	else
-		if (chdir(args[1]) != 0)
-			printf("gerer error\n");
+		if (!(str = ft_getenv("HOME")))
+			return ;
+	manage_env_path(str);
+	chdir(str);
 }
