65391654208462172f7b563f45354806da7fe9c8
AustinFlap <austin.vieira@live.fr>
2020-10-22 03:17:12 +0200

    ft_strcmp

--- srcs/utils/ft_strcmp.c
+++ srcs/utils/ft_strcmp.c
@@ -19,22 +19,20 @@ int		ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] == s2[i])
+			i++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	/*
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
-
+	*/
+	//return(0);
+	return (s1[i] - s2[i]);
 }
-
-/*int i;
-
-i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-	if (s1[i] == s2[i])
-		i++;
-	if (s1[i] < s2[i] || s1[i] > s2[i])
-		return (s1[i] - s2[i]);
-}
-return (0);*/
