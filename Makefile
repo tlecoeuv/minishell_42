3244a127977059dbbbbc942ddcd3d4189bbc9581
AustinFlap <austin.vieira@live.fr>
2020-11-03 01:33:36 +0100

    cd fix

--- Makefile
+++ Makefile
@@ -72,8 +72,7 @@ SRCS = 	srcs/main.c \
 		srcs/utils/ft_strtrim.c \
 		srcs/utils/ft_substr.c \
 		srcs/utils/get_array_size.c \
-		srcs/utils/len_valid_identifier.c \
-		srcs/utils/manage_env_path.c
+		srcs/utils/len_valid_identifier.c 
 
 OBJ = $(SRCS:%.c=%.o)
 
--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -12,115 +12,76 @@
 
 #include "../../includes/minishell.h"
 
-void		replace_dots(char **cd_array)
+char		*get_path_from_env(char car_path)
 {
-	int		i;
-	int		j;
+	char	*path_from_env;
+	char	*path_name;
 
-	i = -1;
-	while (cd_array[++i])
-		if (*cd_array[i] == '.')
-		{
-			j = i;
-			if (!ft_strncmp(cd_array[i], "..", 2))
-			{
-				while (--j)
-				{
-					if (ft_strlen(cd_array[j]))
-					{
-						if (cd_array[j])
-							free(cd_array[j]);
-						cd_array[j] = ft_strdup("");
-						j = 1;
-					}
-				}
-			}
-			if (cd_array[i])
-				free(cd_array[i]);
-			cd_array[i] = ft_strdup("");
-		}
-}
-
-char		*merge_cd_array(char **cd_array)
-{
-	char	*temp;
-	char	*merged;
-	int		i;
-
-	i = 0;
-	if (!(merged = ft_strdup("")))
+	if (car_path == '-')
+		path_name = "OLDPWD";
+	else if (car_path == '~')
+		path_name = "HOME";
+	else
 		return (NULL);
-	while (cd_array[i])
+	if (!(path_from_env = ft_getenv(path_name)))
 	{
-		temp = merged;
-		if (ft_strlen(cd_array[i]))
-		{
-			merged = ft_strjoin_sep(merged, cd_array[i], '/');
-			free(temp);
-		}
-		if (!merged)
-			return (NULL);
-		i++;
-	}
-	return (merged);
-}
-
-char		*transform_pwd(char *prefix, char *arg)
-{
-	char	*new_pwd;
-	char	**cd_array;
-	int		size_before;
-	int		size_after;
-
-	if (!(new_pwd = ft_strjoin_sep(prefix, arg, '/')))
-		return (NULL);
-	cd_array = ft_split(new_pwd, '/');
-	free(new_pwd);
-	if (!cd_array)
-		return (NULL);
-	size_before = get_array_size(cd_array);
-	replace_dots(cd_array);
-	if ((size_after = get_array_size(cd_array)) != size_before)
-	{
-		while (size_before--)
-			if (cd_array[size_before])
-				free(cd_array[size_before]);
-		free(cd_array);
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+		ft_putstr_fd(path_name, STDERR_FILENO);
+		ft_putstr_fd(" not set\n", STDERR_FILENO);
 		return (NULL);
 	}
-	new_pwd = merge_cd_array(cd_array);
-	free_array(cd_array);
-	return (new_pwd);
+	if (!(path_from_env = ft_strdup(path_from_env)))
+		return (NULL);
+	return (path_from_env);
 }
 
 char		*get_new_pwd(char *arg)
 {
-	char	*new_pwd;
-	char	*prefix;
+	char	*from_home;
+	char	*path;
+	char	*result;
 
-	if (*arg == '/')
-		prefix = "";
-	else if (*arg == '~')
+	from_home = NULL;
+	if (*arg == '~')
 	{
-		prefix = ft_getenv("HOME");
+		if (!(from_home = get_path_from_env('~')))
+			return (NULL);
 		arg++;
 	}
-	else if (!ft_strcmp(arg, "-"))
+	if (!(path = ft_strdup(arg)))
+		return (NULL);
+	if (from_home)
 	{
-		ft_putstr_fd(ft_getenv("OLDPWD"), STDOUT_FILENO);
-		ft_putstr_fd("\n", STDOUT_FILENO);
-		return (ft_strdup(ft_getenv("OLDPWD")));
+		if (!(result = ft_strjoin_sep(from_home, path, '/')))
+			return (NULL);
+		free(from_home);
+		free(path);
 	}
 	else
-		prefix = ft_getenv("PWD");
-	if (!(new_pwd = transform_pwd(prefix, arg)))
-		return (NULL);
-	if (!*new_pwd)
+		result = path;
+	return (result);
+}
+
+void		change_directory(char *new_pwd, char *arg)
+{
+	char	*oldpwd;
+	char	*pwd;
+
+	if (chdir(new_pwd) == -1)
 	{
-		free(new_pwd);
-		new_pwd = ft_strdup("/");
+		g_sh.status = 1;
+		error("cd", arg);
 	}
-	return (new_pwd);
+	else
+	{
+		g_sh.status = 0;
+		if (!(oldpwd = ft_strdup(ft_getenv("PWD"))))
+			return ;
+		if (!(pwd = getcwd(NULL, 0)))
+			return ;
+		set_env_var("OLDPWD", oldpwd);
+		set_env_var("PWD", pwd);
+	}
 }
 
 void		ft_cd(char **args)
@@ -130,25 +91,24 @@ void		ft_cd(char **args)
 
 	size = get_array_size(args) - 1;
 	if (size > 1)
-	{
 		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
-		return ;
-	}
-	if (size)
-		new_pwd = get_new_pwd(args[size]);
-	else
-		new_pwd = ft_strdup(ft_getenv("HOME"));
-	if (!new_pwd)
-		return ;
-	if (chdir(new_pwd) == -1)
-	{
-		g_sh.status = 1;
-		error("cd", args[size]);
-	}
 	else
 	{
-		g_sh.status = 0;
-		manage_env_path();
+		if (!ft_strcmp("-", args[size]))
+		{
+			if ((new_pwd = get_path_from_env('-')))
+			{
+				ft_putstr_fd(ft_getenv("OLDPWD"), STDOUT_FILENO);
+				ft_putstr_fd("\n", STDOUT_FILENO);
+			}
+		}
+		else if (size)
+			new_pwd = get_new_pwd(args[size]);
+		else
+			new_pwd = get_path_from_env('~');
+		if (!new_pwd)
+			return ;
+		change_directory(new_pwd, args[size]);
+		free(new_pwd);
 	}
-	free(new_pwd);
 }
