3d3774de2aeb3627895e9cc7b796f178be7411a4
AustinFlap <austin.vieira@live.fr>
2020-11-04 17:06:19 +0100

    export without args

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -12,8 +12,77 @@
 
 #include "../../includes/minishell.h"
 
+
+int			get_len_name(char *env_var)
+{
+	int		len;
+
+	len = 0;
+	while (env_var[len] && env_var[len] != '=')
+		len++;
+	return (len);
+}
+
+void		get_ascii_order(int size, int *order)
+{
+	int		i;
+	int		temp;
+	int		len_a;
+	int		len_b;
+
+	i = -1;
+	while (++i < size)
+		order[i] = i;
+	i = -1;
+	while (++i + 1 < size)
+	{
+		len_a = get_len_name(g_sh.env[order[i]]);
+		len_b = get_len_name(g_sh.env[order[i + 1]]);
+		if (len_a > len_b)
+			len_a = len_b;
+		if (ft_strncmp(g_sh.env[order[i]], g_sh.env[order[i + 1]], len_a) > 0)
+		{
+			temp = order[i];
+			order[i] = order[i + 1];
+			order[i + 1] = temp;
+			i = -1;
+		}
+	}
+}
+
+void		print_export_env()
+{
+	int			i;
+	int			len;
+	int			size;
+	int			*order;
+
+	size = get_array_size(g_sh.env);
+	if (!(order = ft_calloc(sizeof(int), size)))
+		return ;
+	get_ascii_order(size, order);
+	i = -1;
+	while (++i + 1 < size)
+	{
+		len = get_len_name(g_sh.env[order[i]]);
+		ft_putstr_fd("export ", STDOUT_FILENO);
+		write(STDOUT_FILENO, g_sh.env[order[i]], len + 1);
+		if (g_sh.env[order[i]][len] == '=')
+		{
+			ft_putstr_fd("\"", STDOUT_FILENO);
+			ft_putstr_fd(&g_sh.env[order[i]][len + 1], STDOUT_FILENO);
+			ft_putstr_fd("\"", STDOUT_FILENO);
+		}
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	}
+}
+
 void		ft_export(char **args)
 {
+	int		size;
+
 	(void)args;
-	printf("WIP\n");
+	size = get_array_size(args) - 1;
+	if (!size)
+		print_export_env();
 }
