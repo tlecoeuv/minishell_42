809b499003593664410c2c3d257f55dee49231ab
AustinFlap <austin.vieira@live.fr>
2020-11-17 18:25:48 +0100

    cleaner leak test

--- test_leak.sh
+++ test_leak.sh
@@ -14,8 +14,10 @@ while true; do
 			printf "\e[91m\n\n\n------------VALGRIND OUTPUT\n"
 			leak=`grep -A 5 "LEAK SUMMARY" valgrind-out.txt | grep -Po '^.{10}\K.*'`
 			printf "\e[91m$leak"
+			no_leak=`grep -A 5 "All heap blocks were freed -- no leaks are possible" valgrind-out.txt | grep -Po '^.{8}\K.*'`
+			printf "\e[91m$no_leak"
 			printf "\n"
-			error=`grep "ERROR SUMMARY" valgrind-out.txt | grep -Po '.{10}\K.*'`
+			error=`grep "ERROR SUMMARY" valgrind-out.txt | grep -Po '.{8}\K.*'`
 			printf "\e[91m$error"
 			printf "\n\n";;
         [Cc]* ) printf "\e[37m" && cat valgrind-out.txt;;
