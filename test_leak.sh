71561ee1de7c3d4ed0878769b14ca3edfaf1e9ad
AustinFlap <austin.vieira@live.fr>
2020-11-21 04:54:14 +0100

    1 leak trouve

--- test_leak.sh
+++ test_leak.sh
@@ -1,7 +1,7 @@
 printf "\e[32m"
 make
 while true; do
-    printf "\e[32mValgrind minishell (Vv), Cat full report (Cc), Make re (Mm), Exit (Ee)"
+    printf "\e[32mValgrind minishell (Vv), Cat full report (Cc), Infinite mode (Ii), Make re (Mm), Exit (Ee) : "
 	read -p "" choice
     case $choice in
         [Vv]* ) 
@@ -11,18 +11,39 @@ while true; do
 			 --track-origins=yes \
 			 --verbose \
 			 --log-file=valgrind-out.txt ./minishell
-			printf "\e[91m\n\n\n------------VALGRIND OUTPUT\n"
+			printf "\e[91m"
+			printf "VALGRIND OUTPUT\n"
 			leak=`grep -A 5 "LEAK SUMMARY" valgrind-out.txt | grep -Po '^.{10}\K.*'`
 			printf "\e[91m$leak"
 			no_leak=`grep "All heap blocks were freed -- no leaks are possible" valgrind-out.txt | grep -Po '^.{8}\K.*'`
 			printf "\e[91m$no_leak"
 			printf "\n"
 			error=`grep "ERROR SUMMARY" valgrind-out.txt | grep -Po '.{8}\K.*'`
-			printf "\e[91m$error"
-			printf "\n\n";;
-        [Cc]* ) printf "\e[37m" && cat valgrind-out.txt;;
-        [Mm]* ) printf "\e[32m" && make re;;
-        [Ee]* ) exit;;
-        * ) printf "\e[32mpakompri" && printf "\n";;
+			printf "\e[91m$error\n";;
+        [Ii]* )
+			while true; do
+				printf "\e[94m"
+				valgrind --leak-check=full \
+				 --show-leak-kinds=all \
+				 --track-origins=yes \
+				 --verbose \
+				 --log-file=valgrind-out.txt ./minishell
+				printf "\e[91m------------VALGRIND OUTPUT\n"
+				leak=`grep -A 5 "LEAK SUMMARY" valgrind-out.txt | grep -Po '^.{10}\K.*'`
+				printf "\e[91m$leak"
+				no_leak=`grep "All heap blocks were freed -- no leaks are possible" valgrind-out.txt | grep -Po '^.{8}\K.*'`
+				printf "\e[91m$no_leak"
+				printf "\n"
+				error=`grep "ERROR SUMMARY" valgrind-out.txt | grep -Po '.{8}\K.*'`
+				printf "\e[91m$error\n"
+			done;;
+        [Cc]* )
+			printf "\e[37m" && cat valgrind-out.txt;;
+        [Mm]* )
+			printf "\e[32m" && make re;;
+        [Ee]* )
+			exit;;
+        * )
+			printf "\e[32mpakompri" && printf "\n";;
     esac
 done
